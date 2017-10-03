3d99b3337445b765cbcd6c7512b6446fff69cc2c
Mikerah <mikerah14@gmail.com>
2017-10-03 09:34:47 -0400

    Created basic GUI

--- Offline/Offline/GeneratedFiles/ui_offline.h
+++ Offline/Offline/GeneratedFiles/ui_offline.h
@@ -14,10 +14,14 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLCDNumber>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTimeEdit>
 #include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -25,9 +29,14 @@ QT_BEGIN_NAMESPACE
 class Ui_OfflineClass
 {
 public:
+    QWidget *centralWidget;
+    QLCDNumber *timerDisplay;
+    QTimeEdit *setTimeField;
+    QPushButton *startButton;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *OfflineClass)
@@ -35,15 +44,33 @@ public:
         if (OfflineClass->objectName().isEmpty())
             OfflineClass->setObjectName(QStringLiteral("OfflineClass"));
         OfflineClass->resize(600, 400);
+        centralWidget = new QWidget(OfflineClass);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        timerDisplay = new QLCDNumber(centralWidget);
+        timerDisplay->setObjectName(QStringLiteral("timerDisplay"));
+        timerDisplay->setGeometry(QRect(180, 70, 241, 101));
+        setTimeField = new QTimeEdit(centralWidget);
+        setTimeField->setObjectName(QStringLiteral("setTimeField"));
+        setTimeField->setGeometry(QRect(210, 200, 171, 31));
+        startButton = new QPushButton(centralWidget);
+        startButton->setObjectName(QStringLiteral("startButton"));
+        startButton->setGeometry(QRect(260, 270, 91, 51));
+        verticalLayoutWidget = new QWidget(centralWidget);
+        verticalLayoutWidget->setObjectName(QStringLiteral("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(120, 10, 381, 331));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        OfflineClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(OfflineClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 21));
         OfflineClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(OfflineClass);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        OfflineClass->addToolBar(mainToolBar);
-        centralWidget = new QWidget(OfflineClass);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        OfflineClass->setCentralWidget(centralWidget);
+        OfflineClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(OfflineClass);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         OfflineClass->setStatusBar(statusBar);
@@ -56,6 +83,7 @@ public:
     void retranslateUi(QMainWindow *OfflineClass)
     {
         OfflineClass->setWindowTitle(QApplication::translate("OfflineClass", "Offline", Q_NULLPTR));
+        startButton->setText(QApplication::translate("OfflineClass", "PushButton", Q_NULLPTR));
     } // retranslateUi
 
 };
